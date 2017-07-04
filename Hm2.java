40637d5caf85c24b031e39f1a27f9e4298b15077
NightlyHunter <nightlyhunter@gmail.com>
2017-07-04 15:50:24 +0300

    Improved

--- Hm2.java
+++ Hm2.java
@@ -9,6 +9,7 @@ public class Hm2 {
         Scanner city = new Scanner(System.in);
         Scanner age = new Scanner(System.in);
         Scanner hobby = new Scanner(System.in);
+        Scanner var = new Scanner(System.in);
 
         System.out.print("Name:");
         String line1 = name.nextLine();
@@ -18,9 +19,12 @@ public class Hm2 {
         int line3 = age.nextInt();
         System.out.print("Hobby:");
         String line4 = hobby.nextLine();
+        System.out.println("Выбирете вариант вывода: \n1 - табличный; \n2 - текстовый; \n3 - иной.");
+        int line5 = var.nextInt();
 
-        System.out.println("Name:" + line1 + "\nCity:"+ line2+"\nAge:"+line3+"\nHobby:"+line4);
-        System.out.println("Человек по имени " + line1 + " живет в городе " + line2 + ".\nЭтому человеку " +line3+ " лет и любит он заниматься " +line4+ ".");
-        System.out.println(line1 + " - name\n" + line2 + " - city\n" + line3 + " - age\n" + line4 + " - hobby");
+        if (line5==1)System.out.println("Name:" + line1 + "\nCity:"+ line2+"\nAge:"+line3+"\nHobby:"+line4);
+        if (line5==2)System.out.println("Человек по имени " + line1 + " живет в городе " + line2 + ".\nЭтому человеку " +line3+ " лет и любит он заниматься " +line4+ ".");
+        if (line5==3)System.out.println(line1 + " - name\n" + line2 + " - city\n" + line3 + " - age\n" + line4 + " - hobby");
     }
+
 }
